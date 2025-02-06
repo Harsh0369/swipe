0429eaac2c42f9a846c82a6ed9389f655acfed4a
Harsh <harshagarwal81528@gmail.com>
2025-02-06 22:06:01 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './config/db.js';
+
 //routes
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -20,5 +22,6 @@ app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {    
-    console.log('Server started on port: '+PORT);
+    console.log('Server started on port: ' + PORT);
+    connectDB();
 });
